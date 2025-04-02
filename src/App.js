534316b5a00b02d1d40c3805dbb9f086549c4852
Rahul Kumar <rahulkumar03585@gmail.com>
2025-04-03 03:33:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import React, { useEffect } from 'react';
-import ReactGA from 'react-ga4';
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
-  const location = useLocation();  // useLocation hook to track route changes
-  useEffect(() => {
-    // Initialize Google Analytics
-    ReactGA.initialize('G-M8E6Z3NGZV'); // Replace with your GA Measurement ID
-
-    // Track page views whenever the location changes (i.e., whenever route changes)
-    ReactGA.send('pageview', location.pathname + location.search);
-  }, [location]);  // This hook runs whenever location changes (on route change)
   return (
     <>
       <Routes>
